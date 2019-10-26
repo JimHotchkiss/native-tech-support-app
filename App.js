03ac7dc3651f356b8384d7bf5ede2b938fb71ec0
github username <github email address>
2019-10-26 16:23:11 -0700

    Basic stubbing out app

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Button,
+  Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Money Settings Native App</Text>
-      <Text>Designed to help the helpers</Text>
-    </View>
-  );
+export default class App extends Component {
+
+  render() {
+
+    return (
+
+      <View style={styles.container}> 
+        <View style={styles.view}>
+          <Text style={styles.text}>
+            1688
+          </Text>
+        </View>
+        <View style={styles.view}>
+          <Text style={styles.text}>
+            1588
+          </Text>
+        </View>
+        <View style={styles.view}>
+          <Text style={styles.text}>
+            1488
+          </Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    //flex: 1,
+    marginTop: 30,
+    backgroundColor: '#A2A19E',
     alignItems: 'center',
     justifyContent: 'center',
+    height: '100%'
   },
+  view: {
+    width: '70%',
+    height: 'auto',
+    backgroundColor: '#FDCB43',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+    borderRadius: 7,
+  },
+  text: {
+    padding: 20,
+    fontSize: 20,
+
+  }
 });
---
+++ data/Cameras.js
@@ -0,0 +1,21 @@
+const ccu = [
+  {
+    sixteen: {
+      id: '1',
+      name: '1688' 
+    }
+  },
+  {
+    fifteen: {
+      id: '2',
+      name: '1588'
+    }
+  },
+  {
+    fourteen: {
+      id: '3',
+      name: '1488'
+    }
+  }
+  
+]
